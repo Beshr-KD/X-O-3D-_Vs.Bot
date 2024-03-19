d3d7958cb69bc824c36cd009d83ab8980c5ee12a
Beshr Alkeddeh <160949900+Beshr-KD@users.noreply.github.com>
2024-03-19 14:39:15 +0300

    Update dataste.py

--- dataste.py
+++ dataste.py
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-import pandas as pd
-import numpy as np
 
-# قراءة ملف Excel
-df = pd.read_csv('D:\study\project\dataset.csv')
-
-# تغيير ترتيب الصفوف بشكل عشوائي
-df = df.sample(frac=1, random_state=42).reset_index(drop=True)
-
-=======
 import pandas as pd
 import numpy as np
 
