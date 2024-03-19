83f42bff67268f16e23d9bca4b3d88954e026bb8
Beshr-KD <beshralkeda698@gmail.com>
2024-03-19 14:30:21 +0300

    ccccc

--- dataste.py
+++ dataste.py
@@ -7,5 +7,3 @@ df = pd.read_csv('D:\study\project\dataset.csv')
 # تغيير ترتيب الصفوف بشكل عشوائي
 df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 
-# حفظ التغييرات في ملف Excel جديد
-df.to_csv('D:\study\project\dataset2.csv', index=False)
